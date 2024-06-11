962b2d78a65f6f3a815ec16442d68064fe658773
AmjadAfzalAhmed <abhutto2006@gmail.com>
2024-06-11 22:01:59 +0500

    task completed

---
+++ index.js
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+function countInit(seconds) {
+    const targetTime = new Date().getTime() + seconds * 1000;
+    const timerInterval = setInterval(() => {
+        const now = new Date().getTime();
+        const distance = targetTime - now;
+        if (distance <= 0) {
+            clearInterval(timerInterval);
+            console.log(chalk.greenBright.bold('\n \t Countdown completed! \n'));
+            console.log(chalk.redBright.inverse("\t -=-=- Thank You for Using the Countdown Timer -=-=-"));
+        }
+        else {
+            const remainingSeconds = Math.floor(distance / 1000);
+            console.log(`Time left: ${remainingSeconds}s`);
+        }
+    }, 1000);
+}
+console.log(chalk.greenBright.inverse("\t -*-*-*Assalam-o-Alaikum, This code is designed by Amjad Afzal Ahmed-*-*-*"));
+console.log("");
+console.log(chalk.blueBright.bold("\t I Welcome You to CLI Based Countdown Timer App, Executabel in NodeJs Environment "));
+console.log("");
+let answers = inquirer.prompt([
+    {
+        type: 'input',
+        name: 'seconds',
+        message: chalk.greenBright.bold('Enter the countdown duration in seconds (max 20 seconds):'),
+        validate: (value) => {
+            const valid = !isNaN(Number(value)) && Number(value) > 0 && Number(value) <= 20;
+            return valid || 'Please enter a valid number between 1 and 20';
+        }
+    }
+]).then((answers) => {
+    const seconds = parseInt(answers.seconds, 10);
+    console.log(chalk.redBright.bold(`\n Countdown for ${seconds} seconds has started!`));
+    countInit(seconds);
+});
